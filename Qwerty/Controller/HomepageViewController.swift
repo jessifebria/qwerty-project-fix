dda9f8817c9dc73892390e658c0b733636603e36
Nathanael DJ <nathanaeldj@Nathanaels-MacBook-Pro.local>
2021-04-09 18:03:17 +0700

    homepage

---
+++ Qwerty/Controller/HomepageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomepageViewController.swift
+//  Qwerty
+//
+//  Created by Nathanael DJ on 09/04/21.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var botView: UIView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var riwayatView: UITableView!
+    
+    @IBOutlet weak var settingIcon: UIImageView!
+    @IBOutlet weak var berandaIcon: UIImageView!
+    @IBOutlet weak var riwayatIcon: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
