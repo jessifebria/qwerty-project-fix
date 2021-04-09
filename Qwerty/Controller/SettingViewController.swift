4cfeab3f7d82740c03223ef80ba61e04ca7f6b38
Jonathan Herbert <soulofshikigami@yahoo.com>
2021-04-09 22:57:33 +0700

    setting kelarrrr yoooooo

---
+++ Qwerty/Controller/SettingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingViewController.swift
+//  Qwerty
+//
+//  Created by Jonathan Herbert on 09/04/21.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var initial: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var device: UILabel!
+    @IBOutlet weak var circle: UIImageView!
+    
+    var names = UIDevice.current.model
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.sendSubviewToBack(circle)
+        initial.text = String(names.first!)
+        name.text = names
+        device.text = UserService().getUserDevice()
+        //kalo kebalik tinggal diganti yo
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func selesaiButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
