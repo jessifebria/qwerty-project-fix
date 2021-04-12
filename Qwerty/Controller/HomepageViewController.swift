5a4faa0ba2925a307c890212ba22920369b2abca
Nathanael DJ <nathanaeldj@Nathanaels-MacBook-Pro.local>
2021-04-12 16:15:31 +0700

    homepage

--- Qwerty/Controller/HomepageViewController.swift
+++ Qwerty/Controller/HomepageViewController.swift
@@ -7,22 +7,55 @@
 
 import UIKit
 
-class HomepageViewController: UIViewController {
+class HomepageViewController: UIViewController, UICollectionViewDelegate {
     @IBOutlet weak var topView: UIView!
     @IBOutlet weak var botView: UIView!
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var riwayatView: UITableView!
-    
+
     @IBOutlet weak var settingIcon: UIImageView!
     @IBOutlet weak var berandaIcon: UIImageView!
     @IBOutlet weak var riwayatIcon: UIImageView!
+    
+    @IBOutlet weak var dailyCount: UILabel!
+    
+    var riwayatData = HistoryService().getHistory(filter: "Day")
+    var boxContent = DetailKalimatViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dailyCount.text = "\(riwayatData.count)"
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
-    
+}
+    extension HomepageViewController : UICollectionViewDataSource {
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return riwayatData.count
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "riwayatHariIniCell", for: indexPath) as! boxCollectionViewCell
+            let riwayat = riwayatData[indexPath.row]
+            cell.riwayatHariIni = riwayat
+            return cell
+            
+            
+        }
+        
+        func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 1
+        }
+        
+       
+    }
 
     /*
     // MARK: - Navigation
@@ -34,4 +67,4 @@ class HomepageViewController: UIViewController {
     }
     */
 
-}
+
---
+++ Qwerty/Controller/boxCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  boxCollectionViewCell.swift
+//  Qwerty
+//
+//  Created by Nathanael DJ on 12/04/21.
+//
+
+import UIKit
+
+class boxCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var platformLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var entryLabel: UILabel!
+    @IBOutlet weak var profaniteLabel: UILabel!
+    
+    var riwayatHariIni: History? {
+        didSet {
+            self.updateUI ()
+        }
+    }
+    
+    func updateUI() {
+      
+        if let riwayatHariIni = riwayatHariIni {
+            platformLabel.text = riwayatHariIni.platform
+            if let jam = riwayatHariIni.waktu{
+                dateLabel.text = Converter.convertDateToStringHourMinute(date: jam)
+            }
+
+            entryLabel.text = riwayatHariIni.kalimat
+            profaniteLabel.text = riwayatHariIni.kataKotor
+            //colorView.backgroundColor = course.color
+        } else {
+            return
+            //colorView.backgroundColor = nil
+        }
+       
+    }
+    
+}
+
+
