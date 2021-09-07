01080b59346120eb85e3a33bcb070c4ad273f138
Jessi Febria <jfebria2302@gmail.com>
2021-09-07 21:38:42 +0700

    temp

--- SafeTypeKeyboard/KeyboardButton.swift
+++ SafeTypeKeyboard/KeyboardButton.swift
@@ -15,9 +15,59 @@ final class KeyboardButton: UIButton {
     }
 
     func setup() {
-        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        self.layer.shadowColor = KeyboardColors.shadowNormal?.cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+        self.layer.masksToBounds = false
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event:     UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        selectedSetup()
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        normalSetup()
+        #error("FAILED DISINI, HARUSNYA PAS MOVED BS TRIGGER KE YG LAIN")
+        super.touchesEnded(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event:     UIEvent?) {
+        normalSetup()
+        super.touchesEnded(touches, with: event)
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        normalSetup()
+        super.touchesCancelled(touches, with: event)
+    }
+    
+    func selectedSetup(){
+        self.backgroundColor = KeyboardColors.bgBasicButtonSelected
+        self.layer.shadowColor = KeyboardColors.shadowSelected?.cgColor
+        self.isHighlighted = false
+    }
+    
+    func normalSetup(){
+        self.backgroundColor = KeyboardColors.bgBasicButton
+        self.layer.shadowColor = KeyboardColors.shadowNormal?.cgColor
+        self.titleLabel?.textColor = UIColor.label
+    }
+    
+}
+
+final class KeyboardSpecButton: UIButton {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setup()
+    }
+
+    func setup() {
+        self.layer.shadowColor = KeyboardColors.shadowNormal?.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+//        self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0.0
         self.layer.masksToBounds = false
     }
--- SafeTypeKeyboard/KeyboardColors.swift
+++ SafeTypeKeyboard/KeyboardColors.swift
@@ -12,5 +12,9 @@ struct KeyboardColors {
     static let bgBasicButton = UIColor(named: "BGBasicButton")
     static let bgSpecButton = UIColor(named: "BGSpecButton")
     static let bgKeyboard = UIColor(named: "BGKeyboard")
+    static let bgBasicButtonSelected = UIColor(named: "BGBasicButtonSelected")
+    static let bgSpecButtonSelected = UIColor(named: "BGSpecButtonSelected")
+    static let shadowNormal = UIColor(named: "ShadowNormal")
+    static let shadowSelected = UIColor(named: "ShadowSelected")
     
 }
