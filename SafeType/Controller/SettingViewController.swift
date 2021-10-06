58175660628f3fc4782e9b4ba77ae606d2af4b8c
Jessi Febria <jfebria2302@gmail.com>
2021-10-06 09:07:54 +0700

    temp

--- SafeType/Controller/SettingViewController.swift
+++ SafeType/Controller/SettingViewController.swift
@@ -9,24 +9,79 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
-    @IBOutlet weak var initial: UILabel!
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var device: UILabel!
-    @IBOutlet weak var circle: UIImageView!
+    @IBOutlet weak var settingsTableView: UITableView!
+    var settingsItems = [SettingsItem]()
     
-    var names = UIDevice.current.name
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.sendSubviewToBack(circle)
-        initial.text = String(names.first!)
-        name.text = names
-        device.text = UserService().getUserDevice()
-        //kalo kebalik tinggal diganti yo
-        // Do any additional setup after loading the view.
+        for item in SettingsItem.allCases {
+            settingsItems.append(item)
+        }
+        
+        settingsTableView.delegate = self
+        settingsTableView.dataSource = self
+        settingsTableView.tableFooterView = UIView()
+        self.title = "Pengaturan"
+        self.navigationController?.setupNavBar()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+        
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.navigationController?.changeBackground()
     }
     
-    @IBAction func selesaiButton(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    @IBAction func deleteAllButton(_ sender: Any) {
+        showDeleteAlert()
     }
+    
+    func showDeleteAlert() {
+        let alert = UIAlertController(title: "Apakah Anda yakin?", message: "Semua data akan terhapus dan tidak dapat dikembalikan.", preferredStyle: UIAlertController.Style.alert)
+       
+        let cancel = UIAlertAction(title: "Tidak", style: .cancel, handler: { (action : UIAlertAction!) -> Void in
+            
+        })
+        let yesAction = UIAlertAction(title: "Ya", style: .default, handler: { (action) -> Void in
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "DeleteAllViewController") as! DeleteAllViewController
+            vc.modalPresentationStyle = .fullScreen //or .overFullScreen for transparency
+            self.present(vc, animated: true, completion: nil)
+        })
+        alert.addAction(cancel)
+        alert.addAction(yesAction)
+        alert.preferredAction = yesAction
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch settingsItems[indexPath.row] {
+        case .KataTerdeteksi:
+            let vc = self.storyboard?.instantiateViewController(withIdentifier: "KataTerdeteksiViewController") as! KataTerdeteksiViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+        case .Bantuan:
+            print("bantuan")
+        }
+    }
+}
+
+extension SettingViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "settingsCell", for: indexPath) as! SettingsTableViewCell
+        cell.settingsItem = settingsItems[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 115
+    }
+    
     
 }
