e94abc32f74065faae0876d157ba6eaf5a043449
yafonia <yafonia@gmail.com>
2022-05-29 17:52:37 +0700

    add bantuan page

--- SafeType/Cell Controller/BantuanTableViewCell.swift
+++ SafeType/Cell Controller/BantuanTableViewCell.swift
@@ -9,34 +9,48 @@ import UIKit
 
 class BantuanTableViewCell: UITableViewCell {
     
+    let questionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.numberOfLines = 3
+        return label
+    }()
+    
+    let downChevronButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        button.frame.size = CGSize(width: 20.0, height: 20.0)
+        return button
+    }()
+    
     var bantuan: Bantuan? {
         didSet {
             self.updateUI()
         }
     }
+
     private func updateUI() {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
-        label.center = CGPoint(x: 160, y: 285)
-        label.textAlignment = .center
-        label.text = bantuan?.question
-        self.addSubview(label)
-    }
-//    
-//    var bantuan: Bantuan? {
-//        didSet {
-//            kataKotor.text = kata?.kata
-//        }
-//    }
-//
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-//
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+        self.contentView.addSubview(questionLabel)
+        self.contentView.addSubview(downChevronButton)
+        
+        let downChevronButtonConstraints = [
+            downChevronButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            downChevronButton.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20)
+        ]
+        NSLayoutConstraint.activate(downChevronButtonConstraints)
 
-        // Configure the view for the selected state
+        questionLabel.text = bantuan?.question
+        
+        let questionLabelConstraints = [
+            questionLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            questionLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            questionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            questionLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            questionLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -80)
+        ]
+        
+        NSLayoutConstraint.activate(questionLabelConstraints)
     }
-
 }
